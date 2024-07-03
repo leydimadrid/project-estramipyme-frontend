e41e50c5b258d19fbd0709993637204b0ebc2961
leonard <villegas.leonard@gmail.com>
2024-07-03 11:42:09 -0500

    Add next and back function

--- index.js
+++ index.js
@@ -22,25 +22,53 @@ const cerrarModal = function () {
 
 closeBtn.addEventListener("click", cerrarModal);
 
+let currentBtn = 1;
+const numberOfSlides = modalContent.length;
 
-//navigation
-navContainer.addEventListener("click", function (e) {
-  console.log("click", e.target.closest(".form-link"));
-  const clicked = e.target.closest(".form-link");
-  if (!clicked) return;
+const btnLeft = document.querySelector(".btn-back");
+const btnRight = document.querySelector(".btn-next");
 
-  // console.log(navbtns)
+const goToBtn = function (index) {
   // activar el boton
+
+  const clicked = document.querySelector(`.form-link[data-content="${index}"]`);
+
   navbtns.forEach(
     btn => btn.classList.remove("form__link--active")
   )
   clicked.classList.add("form__link--active");
-
+  currentBtn = index;
   // activar el contenido
   modalContent.forEach(
     cnt => cnt.classList.remove("modal__content--active")
   );
   console.log(clicked.dataset.content)
   document.querySelector(`.modal__content--${clicked.dataset.content}`).classList.add("modal__content--active")
+}
+
+
+//navigation
+navContainer.addEventListener("click", function (e) {
+  console.log("click", e.target.closest(".form-link"));
+  const clicked = e.target.closest(".form-link");
+  if (!clicked) return;
+  goToBtn(clicked.dataset.content);
+  currentBtn = clicked.dataset.content;
 })
 
+
+
+btnRight.addEventListener("click", function () {
+  if (currentBtn < numberOfSlides) {
+    currentBtn++;
+    goToBtn(currentBtn)
+  } else return;
+})
+
+btnLeft.addEventListener("click", function () {
+  if (currentBtn > 1) {
+    currentBtn--;
+    goToBtn(currentBtn)
+  } else return;
+
+})
