1a38aee9af0b6e28c15781a24ca6b353877a8430
Angiea18 <angie.a.zapata@gmail.com>
2024-08-15 20:42:33 -0500

    Actualizaciones y validación de respuestas

--- Angular/estramipyme/src/app/app.component.ts
+++ Angular/estramipyme/src/app/app.component.ts
@@ -1,31 +1,39 @@
-import {Component, ElementRef, inject, OnInit, signal} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
-import {GraphsComponent} from './components/graphs/graphs.component'
+import { Component, ElementRef, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { GraphsComponent } from './components/graphs/graphs.component';
 
+import { Question } from '@models/question.model';
+import { RenderFormDirective } from './directives/render-form.directive';
+import { FooterComponent } from './pages/components/footer/footer.component';
+import { GlobalProviderService } from '@services/global-provider.service';
+import { GraphCircleComponent } from './components/graph-circle/graph-circle.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-import {Question} from "@models/question.model";
-import {RenderFormDirective} from "./directives/render-form.directive";
-import {FooterComponent} from "./pages/components/footer/footer.component";
-import {GlobalProviderService} from "@services/global-provider.service";
-import {GraphCircleComponent} from "./components/graph-circle/graph-circle.component";
-import {LoginComponent} from "./pages/login/login.component";
-import {RegisterComponent} from "./pages/register/register.component";
-
-
-type Answers = {} | {
-  [key: string | number]: string;
-}
+type Answers =
+  | {}
+  | {
+      [key: string | number]: string;
+    };
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, GraphsComponent, RenderFormDirective, FooterComponent, GraphCircleComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    GraphsComponent,
+    RenderFormDirective,
+    FooterComponent,
+    GraphCircleComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-
   isLoged = signal<boolean>(false);
   mobileOpen = signal<boolean>(false);
   progress = signal(0);
@@ -49,110 +57,147 @@ export class AppComponent implements OnInit {
   }
 
   toggleLogin() {
-    this.isLoged.update(prevValue => !prevValue)
+    this.isLoged.update((prevValue) => !prevValue);
   }
 
   toggleMobileOpen() {
-    this.mobileOpen.update(prevValue => !prevValue)
+    this.mobileOpen.update((prevValue) => !prevValue);
   }
 
   ngOnInit() {
-    this.globalProvider.IsLogged$.subscribe(value => {
-      console.log("is logged in provider")
-      this.isLoged.set(value)
-    })
+    this.globalProvider.IsLogged$.subscribe((value) => {
+      console.log('is logged in provider');
+      this.isLoged.set(value);
+    });
 
-    this.globalProvider.Progress$.subscribe(value => {
+    this.globalProvider.Progress$.subscribe((value) => {
       this.progress.set(value);
-    })
+    });
     //const startTest = document.querySelector(".start-test");
-    console.log("elemento nava")
-    this.#navLinks = this.el.nativeElement.querySelector(".nav__links");
-    this.#nav = this.el.nativeElement.querySelector(".nav");
+    console.log('elemento nava');
+    this.#navLinks = this.el.nativeElement.querySelector('.nav__links');
+    this.#nav = this.el.nativeElement.querySelector('.nav');
     this.#navHeight = this.#nav.getBoundingClientRect().height;
-    const startTest = this.el.nativeElement.querySelector(".start-test")
-    this.#header = this.el.nativeElement.querySelector(".header");
-    this.#allSections = this.el.nativeElement.querySelectorAll(".section");
-    this.#progress = document.getElementById("progreso");
+    const startTest = this.el.nativeElement.querySelector('.start-test');
+    this.#header = this.el.nativeElement.querySelector('.header');
+    this.#allSections = this.el.nativeElement.querySelectorAll('.section');
+    this.#progress = document.getElementById('progreso');
 
-
-    startTest.addEventListener("click", this._handlerStartTest.bind(this))
+    startTest.addEventListener('click', this._handlerStartTest.bind(this));
     //cargar formularios desde json server
     // this._fetchData()
 
     // Hacer el fade in de la barra de navegacón
-    this.#nav.addEventListener("mouseover", this._handleHover.bind(0.5));
-    this.#nav.addEventListener("mouseout", this._handleHover.bind(1));
-    const sectionObserver = new IntersectionObserver(this._revealSection.bind(this), {
-      root: null,
-      threshold: 0.15,
-    });
-    this.#navLinks.addEventListener("click", function (e: any) {
+    this.#nav.addEventListener('mouseover', this._handleHover.bind(0.5));
+    this.#nav.addEventListener('mouseout', this._handleHover.bind(1));
+    const sectionObserver = new IntersectionObserver(
+      this._revealSection.bind(this),
+      {
+        root: null,
+        threshold: 0.15,
+      }
+    );
+    this.#navLinks.addEventListener('click', function (e: any) {
       e.preventDefault();
 
       // Matching strategy
-      if (e.target.classList.contains("nav__link")) {
-        const id = e.target.getAttribute("href");
-        document.querySelector(id).scrollIntoView({behavior: "smooth"});
+      if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
       }
     });
     this.#allSections.forEach(function (section: HTMLHtmlElement) {
       sectionObserver.observe(section);
-      section.classList.add("section--hidden");
+      section.classList.add('section--hidden');
     });
-    const headerObserver = new IntersectionObserver(this._createStickyNav.bind(this), {
-      root: null,
-      threshold: 0,
-      rootMargin: `${this.#navHeight}px`,
-    });
+    const headerObserver = new IntersectionObserver(
+      this._createStickyNav.bind(this),
+      {
+        root: null,
+        threshold: 0,
+        rootMargin: `${this.#navHeight}px`,
+      }
+    );
     headerObserver.observe(this.#header);
 
+    this._setupResultsButton();
+    // this._setupBorrarButton();
   }
 
-  _revealSection(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
-    console.log("reveal section")
+  // _setupBorrarButton() {
+  //   const borrarBtn = this.el.nativeElement.querySelector('#borrarBtn');
+  //   if (!borrarBtn) {
+  //     console.error('Botón "Borrar Respuestas" no encontrado.');
+  //     return;
+  //   }
+
+  //   borrarBtn.addEventListener('click', () => {
+  //     console.log('reset');
+  //     this.resetForm();
+  //     window.location.hash = '#section--1';
+  //     window.location.reload();
+  //   });
+  // }
+
+  // resetForm(): void {
+  //   this.answers = {}; // Limpia las respuestas
+  //   this.progress.set(0); // Reinicia el progreso
+  // }
+
+  _revealSection(
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) {
+    console.log('reveal section');
     const [entry] = entries;
     if (!entry.isIntersecting) return;
     this.el.nativeElement
-      .querySelectorAll(".nav__link")
-      .forEach((link: HTMLElement) => link.classList.remove("selected-link"));
+      .querySelectorAll('.nav__link')
+      .forEach((link: HTMLElement) => link.classList.remove('selected-link'));
     const id: String = entry.target.id;
-    const element: HTMLHtmlElement = this.el.nativeElement.querySelector(`.nav__link[href="#${id}"]`);
+    const element: HTMLHtmlElement = this.el.nativeElement.querySelector(
+      `.nav__link[href="#${id}"]`
+    );
 
-    this.el.nativeElement.querySelector(`.nav__link[href="#${id}"]`)
-      .classList.add("selected-link");
+    this.el.nativeElement
+      .querySelector(`.nav__link[href="#${id}"]`)
+      .classList.add('selected-link');
     // entry.target.classList.remove('section--hidden');
     // observer.unobserve(entry.target);
-  };
+  }
 
   _handleHover(e: any) {
-    if (e.target.classList.contains("nav__link")) {
+    if (e.target.classList.contains('nav__link')) {
       const link = e.target;
-      const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-      const logo = link.closest(".nav").querySelector("img");
+      const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+      const logo = link.closest('.nav').querySelector('img');
 
       siblings.forEach((el: any) => {
         if (el !== link) el.style.opacity = this;
       });
       logo.style.opacity = this;
     }
-  };
+  }
 
   _handlerStartTest(e: MouseEvent) {
-    console.log("event targe")
+    console.log('event targe');
     const target = e.target as unknown as HTMLElement;
     e.preventDefault();
 
-    const id: String | null = target.getAttribute("href");
+    const id: String | null = target.getAttribute('href');
     //console.log(id);
     // console.log(id)
-    this.el.nativeElement.querySelector(id).scrollIntoView({behavior: "smooth"});
+    this.el.nativeElement
+      .querySelector(id)
+      .scrollIntoView({ behavior: 'smooth' });
   }
 
   _getProgress() {
     if (!this.answers) return 0;
     if (!this.numberOfQuestions) return 0;
-    return Math.ceil((Object.keys(this.answers).length / this.numberOfQuestions) * 100);
+    return Math.ceil(
+      (Object.keys(this.answers).length / this.numberOfQuestions) * 100
+    );
   }
 
   // _fetchData() {
@@ -168,11 +213,70 @@ export class AppComponent implements OnInit {
 
   _createStickyNav(entries: any) {
     const [entry] = entries;
-    if (entry.isIntersecting) this.#nav.classList.add("hidden");
-    else this.#nav.classList.remove("hidden");
-  };
+    if (entry.isIntersecting) this.#nav.classList.add('hidden');
+    else this.#nav.classList.remove('hidden');
+  }
 
+  _setupResultsButton() {
+    const showResultsButton = this.el.nativeElement.querySelector(
+      '.section-see-results .btn--show-modal'
+    );
 
+    if (!showResultsButton) {
+      console.error('Botón "Ver resultados" no encontrado.');
+      return;
+    }
+
+    showResultsButton.addEventListener('click', () => {
+      const fieldsets = Array.from(
+        this.el.nativeElement.querySelectorAll('.form-container fieldset')
+      ) as HTMLFieldSetElement[];
+
+      const fieldsetIds = fieldsets.map(
+        (fieldset) => fieldset.getAttribute('data-question-id') || ''
+      );
+
+      const unansweredFieldsets = fieldsetIds.filter(
+        (id) => !this.answers.hasOwnProperty(id)
+      );
+
+      // Resetear cualquier error previo
+      fieldsets.forEach((fieldset) => {
+        fieldset.classList.remove('fieldset-error');
+      });
+
+      if (unansweredFieldsets.length > 0) {
+        const unansweredElements = Array.from(
+          this.el.nativeElement.querySelectorAll(
+            `.form-container fieldset[data-question-id="${unansweredFieldsets.join(
+              '"], .form-container fieldset[data-question-id="'
+            )}"]`
+          )
+        ) as HTMLFieldSetElement[];
+
+        unansweredElements.forEach((fieldset) => {
+          if (fieldset) {
+            fieldset.classList.add('fieldset-error');
+          }
+        });
+
+        if (unansweredElements.length > 0 && unansweredElements[0]) {
+          unansweredElements[0].scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+          });
+        }
+      } else {
+        console.log('Todas las preguntas han sido respondidas.');
+        const radarSection =
+          this.el.nativeElement.querySelector('.section-radar');
+        if (radarSection) {
+          radarSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+          });
+        }
+      }
+    });
+  }
 }
-
-
--- Angular/estramipyme/src/app/services/global-provider.service.ts
+++ Angular/estramipyme/src/app/services/global-provider.service.ts
@@ -1,10 +1,12 @@
-import {computed, effect, inject, Injectable, signal} from '@angular/core';
-import {DataProcService} from "@services/data-proc.service";
-import {BehaviorSubject} from "rxjs";
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { DataProcService } from '@services/data-proc.service';
+import { BehaviorSubject } from 'rxjs';
 
-type Answer = {} | {
-  [key: number]: string;
-}
+type Answer =
+  | {}
+  | {
+      [key: number]: string;
+    };
 
 export interface Circle {
   what: number[];
@@ -12,22 +14,23 @@ export interface Circle {
   why: number[];
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GlobalProviderService {
-  answers = signal<Answer>({})
-  private progress = new BehaviorSubject<number>(0)
+  answers = signal<Answer>({});
+  private progress = new BehaviorSubject<number>(0);
   numberOfQuestions = 51;
-  private dataProc = inject(DataProcService)
+  private dataProc = inject(DataProcService);
 
-  private RadarData: BehaviorSubject<number[]> = new BehaviorSubject([1, 2, 3, 4, 5])
+  private RadarData: BehaviorSubject<number[]> = new BehaviorSubject([
+    1, 2, 3, 4, 5,
+  ]);
   private CircleData: BehaviorSubject<Circle> = new BehaviorSubject({
     what: [90, 10],
     how: [90, 10],
     why: [90, 10],
-  })
+  });
   private isLogged: BehaviorSubject<boolean> = new BehaviorSubject(false);
   Progress$ = this.progress.asObservable();
   RadarData$ = this.RadarData.asObservable();
@@ -42,58 +45,73 @@ export class GlobalProviderService {
   circuloComo!: string[];
   circuloPorQue!: string[];
   scores = computed(() => {
-    console.log("compute scores")
-    console.log(this.answers())
+    console.log('compute scores');
+    console.log(this.answers());
     //console.log(this.cliente)
-    return this.getScores()
-  })
+    return this.getScores();
+  });
 
   constructor() {
     this.dataProc.getItems('http://localhost:3000/questions').subscribe({
-      next: questions => {
+      next: (questions) => {
+        this.numberOfQuestions = questions.length; //Obtener los ids
+        this.cliente = questions
+          .filter((e) => e.section === 'cliente')
+          .map((q) => String(q.id));
+        this.negocio = questions
+          .filter((e) => e.section === 'negocio')
+          .map((q) => String(q.id));
+        this.coherencia = questions
+          .filter((e) => e.section === 'coherencia')
+          .map((q) => String(q.id));
+        this.alineacion = questions
+          .filter((e) => e.section === 'alineacion')
+          .map((q) => String(q.id));
+        this.financiera = questions
+          .filter((e) => e.section === 'financiera')
+          .map((q) => String(q.id));
+        this.circuloQue = questions
+          .filter((e) => e.section === 'circulo' && e.subsection === 'que')
+          .map((q) => String(q.id));
+        this.circuloComo = questions
+          .filter((e) => e.section === 'circulo' && e.subsection === 'como')
+          .map((q) => String(q.id));
+        this.circuloPorQue = questions
+          .filter((e) => e.section === 'circulo' && e.subsection === 'por que')
+          .map((q) => String(q.id));
 
-        this.numberOfQuestions = questions.length;//Obtener los ids
-        this.cliente = questions.filter(e => e.section === 'cliente').map(q => String(q.id));
-        this.negocio = questions.filter(e => e.section === 'negocio').map(q => String(q.id));
-        this.coherencia = questions.filter(e => e.section === 'coherencia').map(q => String(q.id));
-        this.alineacion = questions.filter(e => e.section === 'alineacion').map(q => String(q.id));
-        this.financiera = questions.filter(e => e.section === 'financiera').map(q => String(q.id));
-        this.circuloQue = questions.filter(e => e.section === 'circulo' && e.subsection === "que").map(q => String(q.id));
-        this.circuloComo = questions.filter(e => e.section === 'circulo' && e.subsection === "como").map(q => String(q.id));
-        this.circuloPorQue = questions.filter(e => e.section === 'circulo' && e.subsection === "por que").map(q => String(q.id));
-
-        console.log(this.circuloComo)
-        console.log("circuloComo")
+        console.log(this.circuloComo);
+        console.log('circuloComo');
         //console.log(this.answers())
         //console.log("ruuun")
       },
-      error: err => {
-        console.log(err)
-      }
-    })
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   setLocalStorage(id: number | string, value: string) {
     const newValue = {
-      [id]: value
-    }
+      [id]: value,
+    };
 
     this.answers.update((prevValue) => {
-      return {...prevValue, ...newValue}
-    })
-    localStorage.setItem("estramipyme", JSON.stringify(this.answers()));
-    this.getProgress()
-    this.getRadarData()
+      return { ...prevValue, ...newValue };
+    });
+    localStorage.setItem('estramipyme', JSON.stringify(this.answers()));
+    this.getProgress();
+    this.getRadarData();
     this.getCircleData();
   }
 
   getLocalStorage() {
-    const storedDataString = localStorage.getItem("estramipyme");
+    const storedDataString = localStorage.getItem('estramipyme');
     if (storedDataString == null) return;
     const data: object = JSON.parse(storedDataString);
     if (!data) return;
     this.answers.update(() => {
-      return {...this.answers(), ...data}
+      return { ...this.answers(), ...data };
     });
     this.getProgress();
     this.getRadarData();
@@ -101,9 +119,23 @@ export class GlobalProviderService {
   }
 
   getProgress() {
-    if (!this.answers()) this.progress.next(Math.ceil((Object.keys(this.answers()).length / this.numberOfQuestions) * 100));
-    if (!this.numberOfQuestions) this.progress.next(Math.ceil((Object.keys(this.answers()).length / this.numberOfQuestions) * 100));
-    this.progress.next(Math.ceil((Object.keys(this.answers()).length / this.numberOfQuestions) * 100))
+    if (!this.answers())
+      this.progress.next(
+        Math.ceil(
+          (Object.keys(this.answers()).length / this.numberOfQuestions) * 100
+        )
+      );
+    if (!this.numberOfQuestions)
+      this.progress.next(
+        Math.ceil(
+          (Object.keys(this.answers()).length / this.numberOfQuestions) * 100
+        )
+      );
+    this.progress.next(
+      Math.ceil(
+        (Object.keys(this.answers()).length / this.numberOfQuestions) * 100
+      )
+    );
   }
 
   getScores(): number[] {
@@ -112,12 +144,11 @@ export class GlobalProviderService {
     let coherenciaAcc = 0;
     let alineacionAcc = 0;
     let financieraAcc = 0;
-    const tCliente = +this.cliente.length
-    const tNegocio = +this.negocio.length
-    const tCoherencia = +this.coherencia.length
-    const tAlineacion = +this.alineacion.length
-    const tFinanciera = +this.financiera.length
-
+    const tCliente = +this.cliente.length;
+    const tNegocio = +this.negocio.length;
+    const tCoherencia = +this.coherencia.length;
+    const tAlineacion = +this.alineacion.length;
+    const tFinanciera = +this.financiera.length;
 
     Object.entries(this.answers()).forEach(([key, value]) => {
       //console.log(key, value);
@@ -137,24 +168,22 @@ export class GlobalProviderService {
       if (this.financiera.includes(key)) {
         financieraAcc += Number(value);
       }
-    })
+    });
 
     return [
-      Number((coherenciaAcc / (tCoherencia * 4) * 4).toFixed(2)),
-      Number((financieraAcc / (tFinanciera * 4) * 4).toFixed(2)),
-      Number((clientAcc / (tCliente * 4) * 4).toFixed(2)),
-      Number((alineacionAcc / (tAlineacion * 4) * 4).toFixed(2)),
-      Number((negocioAcc / (tNegocio * 4) * 4).toFixed(2))
+      Number(((coherenciaAcc / (tCoherencia * 4)) * 4).toFixed(2)),
+      Number(((financieraAcc / (tFinanciera * 4)) * 4).toFixed(2)),
+      Number(((clientAcc / (tCliente * 4)) * 4).toFixed(2)),
+      Number(((alineacionAcc / (tAlineacion * 4)) * 4).toFixed(2)),
+      Number(((negocioAcc / (tNegocio * 4)) * 4).toFixed(2)),
     ];
   }
 
   getCircle() {
-
     let comoAcc = 0;
     let queAcc = 0;
     let porQueAcc = 0;
     const total = 4;
-
 
     Object.entries(this.answers()).forEach(([key, value]) => {
       //console.log(key, value);
@@ -168,35 +197,32 @@ export class GlobalProviderService {
       if (this.circuloPorQue.includes(key)) {
         porQueAcc += Number(value);
       }
+    });
 
-    })
-
-    console.log("conteo")
-    console.log(this.circuloQue)
-    console.log(porQueAcc)
-    console.log(comoAcc)
-
+    console.log('conteo');
+    console.log(this.circuloQue);
+    console.log(porQueAcc);
+    console.log(comoAcc);
 
     return {
-      what: [queAcc / (total * 4) * 100, 100 - (queAcc / (total * 4) * 100)],
-      how: [comoAcc / (total * 4) * 100, 100 - (comoAcc / (total * 4) * 100)],
-      why: [porQueAcc / (total * 4) * 100, 100 - (porQueAcc / (total * 4) * 100)],
-    }
+      what: [(queAcc / (total * 4)) * 100, 100 - (queAcc / (total * 4)) * 100],
+      how: [(comoAcc / (total * 4)) * 100, 100 - (comoAcc / (total * 4)) * 100],
+      why: [
+        (porQueAcc / (total * 4)) * 100,
+        100 - (porQueAcc / (total * 4)) * 100,
+      ],
+    };
   }
 
   getRadarData() {
-    this.RadarData.next(
-      this.getScores()
-    )
+    this.RadarData.next(this.getScores());
   }
 
   setLogging(value: boolean) {
-    this.isLogged.next(value)
+    this.isLogged.next(value);
   }
 
   getCircleData() {
-    this.CircleData.next(
-      this.getCircle()
-    )
+    this.CircleData.next(this.getCircle());
   }
 }
