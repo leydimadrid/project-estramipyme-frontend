e68144af3097785f3d7ccce376f44bc689ddf0a5
leydimadrid <leydi.madrid6@gmail.com>
2024-11-13 15:27:30 -0500

    Implementación recuperar contraseña y guardar sesión iniciada

--- estramipyme/src/app/app.routes.ts
+++ estramipyme/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from '@pages/register/register.component';
+import { ResetPasswordComponent } from '@pages/components/reset-password/reset-password.component';
+import { ResetPasswordRequestComponentTsComponent } from '@pages/components/reset-password-request/reset-password-request.component.ts/reset-password-request.component';
 
 export const routes: Routes = [
   // {path: "login", component: LoginComponent},
@@ -13,4 +15,8 @@ export const routes: Routes = [
     path: 'registro',
     component: RegisterComponent,
   },
+  { path: 'reset-password-request',
+    component: ResetPasswordRequestComponentTsComponent },
+  { path: 'reset-password',
+    component: ResetPasswordComponent },
 ];
